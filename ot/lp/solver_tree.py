7c9425b63ac9f87c8aa4144b25983a48be56ee59
Boudjema Ali <ali.boudjema@ens-paris-saclay.fr>
2026-06-16 15:53:34 +0200

    plan transport

--- ot/lp/solver_tree.py
+++ ot/lp/solver_tree.py
@@ -50,7 +50,9 @@ def topological_sort(tree):
     return np.array(topo_order)
 
 
-def tree_wasserstein(tree, length, u_weights, v_weights, topo_order=None):
+def tree_wasserstein(
+    tree, length, u_weights, v_weights, topo_order=None, return_plans=False
+):
     r"""
     Computes the tree wasserstein distance for a given tree between two empirical distributions
 
@@ -64,13 +66,18 @@ def tree_wasserstein(tree, length, u_weights, v_weights, topo_order=None):
         weights of the first empirical distributions
     v_weights : array_like, shape(n)
         weights of the second empirical distributions
-    topo_order : array_like, shape(n)
-        topological order of the tree, optional
+    topo_order : array_like, shape(n), optional
+        topological order of the tree
+    return_plans : bool, optional
+        if True, returns the optimal transport plan between the
+        two distributions, default is False
 
     Returns
     -------
     cost : float
         The tree wasserstein distance
+    plans : coo_matrix, optional
+        If return_plans is True, returns a coo_matrix containing the plan
     """
 
     n = tree.shape[0]
@@ -84,21 +91,88 @@ def tree_wasserstein(tree, length, u_weights, v_weights, topo_order=None):
 
     nx = get_backend(length, u_weights, v_weights)
 
-    u_cumweights = nx.copy(u_weights)
-    v_cumweights = nx.copy(v_weights)
+    mass_dict = {}
+
+    for cur in range(n):
+        if u_weights[cur] != v_weights[cur]:
+            mass_dict[cur] = {cur: u_weights[cur] - v_weights[cur]}
+        else:
+            mass_dict[cur] = {}
+
+    source_plan = []
+    sink_plan = []
+    mass_plan = []
+
+    virt_size = [len(mass_dict[k]) for k in range(n)]
 
     cost = 0
 
-    for i in range(n):
+    depth = nx.zeros(n)
+
+    for i in range(n - 2, -1, -1):
         cur_node = topo_order[i]
+        depth[cur_node] = depth[tree[cur_node]] + length[cur_node]
 
-        cost += length[cur_node] * nx.abs(
-            u_cumweights[cur_node] - v_cumweights[cur_node]
-        )
+    for cur in topo_order:
+        dict_cur = mass_dict[cur]
+        p = tree[cur]
 
-        ancestor = tree[cur_node]
+        if cur != p:
+            dict_p = mass_dict[p]
 
-        u_cumweights[ancestor] = u_cumweights[cur_node] + u_cumweights[ancestor]
-        v_cumweights[ancestor] = v_cumweights[cur_node] + v_cumweights[ancestor]
+            if virt_size[cur] > virt_size[p]:
+                mass_dict[cur], mass_dict[p] = dict_p, dict_cur
+                dict_cur, dict_p = dict_p, dict_cur
+                virt_size[cur], virt_size[p] = virt_size[p], virt_size[cur]
 
-    return cost
+            while len(dict_cur) > 0 and len(dict_p) > 0:
+                node_scur = next(iter(dict_cur))
+                amount_scur = dict_cur[node_scur]
+
+                node_sp = next(iter(dict_p))
+                amount_sp = dict_p[node_sp]
+
+                if (amount_scur > 0) != (amount_sp > 0):
+                    match_amount = min(abs(amount_scur), abs(amount_sp))
+
+                    source = node_scur if amount_scur > 0 else node_sp
+                    sink = node_sp if amount_scur > 0 else node_scur
+
+                    source_plan.append(source)
+                    sink_plan.append(sink)
+                    mass_plan.append(match_amount)
+
+                    length_path = depth[source] + depth[sink] - 2 * depth[p]
+                    cost += match_amount * length_path
+
+                    if amount_scur > 0:
+                        dict_cur[node_scur] -= match_amount
+                        dict_p[node_sp] += match_amount
+                    else:
+                        dict_cur[node_scur] += match_amount
+                        dict_p[node_sp] -= match_amount
+
+                    if dict_cur[node_scur] == 0:
+                        del dict_cur[node_scur]
+
+                    if dict_p[node_sp] == 0:
+                        del dict_p[node_sp]
+
+                else:
+                    dict_p[node_scur] = amount_scur
+                    del dict_cur[node_scur]
+
+            if len(dict_p) == 0:
+                mass_dict[cur], mass_dict[p] = dict_p, dict_cur
+                dict_cur, dict_p = dict_p, dict_cur
+
+            virt_size[p] += virt_size[cur]
+
+    plans = nx.coo_matrix(
+        mass_plan, source_plan, sink_plan, shape=(n, n), type_as=length
+    )
+
+    if return_plans:
+        return cost, plans
+    else:
+        return cost
